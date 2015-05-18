0ccde87ac509282cbbf00515a74ac09d45fa16f7
Tat <tatchx@gmail.com>
2015-05-18 20:23:53 +0530

    adding a dummy stl-reader.js file which exports a read function

---
+++ stl-reader.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+
+  /**
+   * Reads the triangle vertices of an STL file into a Float32Array
+   *
+   * @param  {blob} file The file as a blob
+   * @return {Float23Array}
+   */
+   read: function (file) {
+
+   }
+};
