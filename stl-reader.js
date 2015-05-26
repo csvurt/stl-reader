1c3d11b146183f39510f678613ee937c9ad5ed7c
tat <tatchx@gmail.com>
2015-05-26 20:41:05 +0530

    trying to determine if the STL file is ASCII or binary

--- stl-reader.js
+++ stl-reader.js
@@ -4,7 +4,7 @@
   var StlReader = (function() {
 
     if (typeof DataStream === 'undefined') {
-      DataStream = require('data-stream').DataStream;
+      DataStream = require('./DataStream.js/DataStream.js');
     }
 
     var StlReader = function(options) {
@@ -19,6 +19,14 @@
     StlReader.prototype.read = function(fileData) {
 
       var ds = new DataStream(fileData);
+
+      var skipHeader = ds.readUint8Array(80);
+      var numTriangles = ds.readUint32();
+      if (numTriangles > 0 && numTriangles < Math.pow(2, 32)) {
+        console.log('Binary STL');
+      } else {
+        console.log('ASCII STL');
+      }
     };
 
     return StlReader;
