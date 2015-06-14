654a15248f567f476eef286a96b2143415e5d141
Tat <tatchx@gmail.com>
2015-06-14 12:58:38 +0530

    moving some of the functions out as class functions mostly for readability

--- stl-reader.js
+++ stl-reader.js
@@ -25,39 +25,51 @@
       attribute count at the end */
     StlReader.PER_TRI_SIZE = 4*(4*3)+2;
 
+    /** 
+     * Check if the file is too small to be a valid STL file.
+     *
+     * @param {DataStream} ds the file data as a DataStream
+     * @returns {Boolean} true if the file is too small to be valid
+     */
+    StlReader.isTooSmallToBeValid = function (ds) {
+      ds.seek(0);
+      if (ds.byteLength < StlReader.HEADER_SIZE + StlReader.TRI_COUNT_SIZE) {
+        return true;
+      }
+
+      return false;
+    }
+
+    /** 
+     * Check if the file is a binary STL file.
+     *
+     * @param {DataStream} ds the file data as a DataStream
+     * @returns {Boolean} true if the file is a binary STL file
+     */
+    StlReader.isBinary = function (ds) {
+      ds.seek(0);
+
+      var fileSize = ds.byteLength;
+
+      var skipHeader = ds.readUint8Array(StlReader.HEADER_SIZE);
+      var numTriangles = ds.readUint32();
+
+      if (fileSize == StlReader.HEADER_SIZE + StlReader.TRI_COUNT_SIZE +
+        numTriangles*StlReader.PER_TRI_SIZE) {
+
+        return true;
+      }
+
+      return false;
+    }
+
     /**
      * Reads the triangle vertices of an STL file into a Float32Array
      *
      * @param  {ArrayBuffer} fileData The file as an ArrayBuffer
      * @return {Float23Array}
      */
-    StlReader.prototype.read = function(fileData) {
-
-      function isTooSmallToBeValid(ds) {
-        ds.seek(0);
-        if (ds.byteLength < StlReader.HEADER_SIZE + StlReader.TRI_COUNT_SIZE) {
-          return true;
-        }
-
-        return false;
-      }
-
-      function isBinary(ds) {
-        ds.seek(0);
-
-        var fileSize = ds.byteLength;
-
-        var skipHeader = ds.readUint8Array(StlReader.HEADER_SIZE);
-        var numTriangles = ds.readUint32();
-
-        if (fileSize == StlReader.HEADER_SIZE + StlReader.TRI_COUNT_SIZE +
-          numTriangles*StlReader.PER_TRI_SIZE) {
-
-          return true;
-        }
-
-        return false;
-      }
+    StlReader.prototype.read = function(fileData) { 
 
       if (fileData.byteLength == 0) {
         return null;
@@ -65,11 +77,11 @@
 
       var ds = new DataStream(fileData);
 
-      if (isTooSmallToBeValid(ds)) {
+      if (StlReader.isTooSmallToBeValid(ds)) {
         return null;
       }
 
-      if (isBinary(ds)) {
+      if (StlReader.isBinary(ds)) {
 
       } else {
         var reader = new StlAsciiReader();
