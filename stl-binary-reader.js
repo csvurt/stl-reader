f0848091ededfe06819e6b59e3363480c247da09
Tat <tatchx@gmail.com>
2015-06-19 10:26:09 +0530

    adding a function to read off the binary stl header

--- stl-binary-reader.js
+++ stl-binary-reader.js
@@ -6,6 +6,18 @@
     var StlBinaryReader = function(options) {
     };
 
+    /* Header size is 80 bytes */
+    StlBinaryReader.HEADER_SIZE = 80;
+
+    /**
+     * Reads the binary STL header
+     *
+     * @param  {DataStream} ds the file DataStream object
+     */
+    StlBinaryReader.readHeader = function (ds) {
+      ds.readUint8Array(StlBinaryReader.HEADER_SIZE);
+    };
+
     /**
      * Reads the triangle vertices of a binary STL file into a Float32Array
      *
@@ -14,6 +26,7 @@
      */
     StlBinaryReader.prototype.read = function(ds) {
 
+      StlBinaryReader.readHeader(ds);
     };
 
     return StlBinaryReader;
