489fd7671a5e3770eb625856065aa5f7c8c7648f
Tat <tatchx@gmail.com>
2015-06-20 11:08:32 +0530

    adding a function to loop over the triangles

--- stl-binary-reader.js
+++ stl-binary-reader.js
@@ -19,6 +19,22 @@
     };
 
     /**
+     * Reads the binary STL triangles
+     *
+     * @param  {DataStream} ds the file DataStream object
+     * @returns {Float32Array} the typed array with the interleaved data.
+     */
+    StlBinaryReader.readTriangles = function (ds) {
+
+      var numTri = ds.readUint32();
+      var typedArray = new Float32Array(numTri*3*2*3);
+
+      // TODO: read the triangles into the typed array
+
+      return typedArray;
+    };
+
+    /**
      * Reads the triangle vertices of a binary STL file into a Float32Array
      *
      * @param  {DataStream} ds The file data as a DataStream object
