c1a15c85ff3904531e9c7663ec3748a42fb2669f
Tat <tatchx@gmail.com>
2015-06-20 11:31:03 +0530

    implemented the binary reader

--- stl-binary-reader.js
+++ stl-binary-reader.js
@@ -19,6 +19,41 @@
     };
 
     /**
+     * Copy a vert/normal into an existing array
+     *
+     * @param {Float32Array} vec the vector to copy
+     * @param {Float32Array} arr the array to copy into
+     * @param {number} idx the index from which to start copying
+     */
+    StlBinaryReader.readVec3IntoArray = function (vec, arr, idx) {
+
+      for (var i = 0; i< 3; i++) {
+        arr[idx + i] = vec[i];
+      }
+    };
+
+    /**
+     * Read each individual triangle
+     *
+     * @param {DataStream} ds the file DataStream object
+     * @param {Float32Array} arr the array to read the triangle data into
+     * @param {number} idx the index in the array from where to start
+     */
+    StlBinaryReader.readTriangle = function (ds, arr, idx) {
+
+      var normal = ds.readUint32Array(3);
+      for (var i = 0; i<3; i++) {
+
+        var vert = ds.readUint32Array(3);
+
+        StlBinaryReader.readVec3IntoArray(vert, arr, idx + i*6);
+        StlBinaryReader.readVec3IntoArray(normal, arr, idx + i*6 + 3);
+      }
+
+      ds.readUint16();
+    };
+
+    /**
      * Reads the binary STL triangles
      *
      * @param  {DataStream} ds the file DataStream object
@@ -29,7 +64,9 @@
       var numTri = ds.readUint32();
       var typedArray = new Float32Array(numTri*3*2*3);
 
-      // TODO: read the triangles into the typed array
+      for (var i = 0; i< numTri; i++) {
+        StlBinaryReader.readTriangles(ds, typedArray, idx + i*3*3*2);
+      }
 
       return typedArray;
     };
@@ -38,7 +75,7 @@
      * Reads the triangle vertices of a binary STL file into a Float32Array
      *
      * @param  {DataStream} ds The file data as a DataStream object
-     * @return {Float23Array} contains interleaved vertex normal data
+     * @returns {Float23Array} contains interleaved vertex normal data
      */
     StlBinaryReader.prototype.read = function(ds) {
 
