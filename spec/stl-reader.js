dc40083e6eb790f3bb2acf06d01f64cd45b74142
Tommy Conway <tmycnw@gmail.com>
2015-07-18 09:58:12 -0700

    removing the function exists tests from spec

--- spec/stl-reader.js
+++ spec/stl-reader.js
@@ -7,21 +7,5 @@ describe('StlReader functions', function () {
     var reader = new StlReader();
     expect(reader.read).toBeDefined();
   });
-
-  it ('should have an isBinary function to determine if the STL file is \
-    binary or ASCII', function () {
-    var reader = new StlReader();
-    expect(reader.isBinary).toBeDefined();
-  });
-
-  it ('should have a readBinary function to read binary STL files', function () {
-    var reader = new StlReader();
-    expect(reader.readBinary).toBeDefined();
-  });
-
-  it ('should have a readAscii function to read ascii STL files', function () {
-    var reader = new StlReader();
-    expect(reader.readAscii).toBeDefined();
-  });
   /*jshint multistr:false*/
 });
